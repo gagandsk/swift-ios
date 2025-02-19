c9f7cd85f12dc80417efd4424ffcb46f632ea841
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-19 14:31:13 +0100

    conjuntos: operaciones

--- Conjuntos.playground/Contents.swift
+++ Conjuntos.playground/Contents.swift
@@ -16,3 +16,29 @@ animales.insert("conejo")
 animales.remove("Caballo")
 animales.removeAll()
 print(animales)
+
+
+/*
+ - Unión: Combina los elementos de ambos sets, eliminando duplicados. Usas setA.union(setB).
+ - Intersección: Obtiene los elementos comunes en ambos sets. Se realiza con setA.intersection(setB).
+ - Resta: Devuelve un nuevo set con elementos de setA que no están en setB. Se hace con setA.subtracting(setB).
+ - Diferencia simétrica: Devuelve elementos que están en uno de los sets, pero no en ambos. Se usa setA.symmetricDifference(setB).
+ */
+
+let matematicas = Set<String>(["Juan", "Camila", "Maria", "Andrea", "Jhon"])
+let spanish = Set<String>(["Andres", "Rick", "Camila", "Maria", "Andrea", "Camilo"])
+
+let unionStudents = matematicas.union(spanish)
+print(unionStudents)
+
+let intersectionStudents = matematicas.intersection(spanish)
+print(intersectionStudents)
+
+let substractingMathStudents = matematicas.subtracting(spanish)
+print(substractingMathStudents)
+
+let substractingSpanishStudents = spanish.subtracting(matematicas)
+print(substractingSpanishStudents)
+
+let diff = matematicas.symmetricDifference(spanish)
+print(diff)
